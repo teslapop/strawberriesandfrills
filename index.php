980d85fb7d4658dc15c060b9237809a6be1aa569
teslapop <teslavortex@gmail.com>
2016-05-22 02:26:18 -0700

    some changes to html and css ... pop up java floating thinggies ... new images .... etc

--- index.php
+++ index.php
@@ -8,6 +8,28 @@
 	<link rel='stylesheet' type='text/css' href='styles/global.css'>
 	<!--<link rel='stylesheet' type='text/css' href='styles/StrawberriesAndFrills.css'>-->
 
+
+
+
+	<script>
+	var a = "<div id='temp' class='floater floater_first'>this is a test floater</div>";
+	var counter = 0;
+	function myFunction() {	
+		if(counter > 0){
+			document.getElementById('temp').remove();
+			a = a + "<div id='temp' class='floater'>this is a test floater</div> <div id='demo'></div>";
+
+
+		}
+
+		document.getElementById("demo").innerHTML = a;
+		counter++;
+
+
+	}
+	</script>
+
+
 </head>
 <body>
 	<div id='bgimg'>
@@ -49,16 +71,22 @@
 					<h1>On The Electro-Dynamics of Lolita Fashion</h1><br>
 
 
+					<button onclick="myFunction()">Click me</button>
+
+
+
+
+
 					<p>Lorem ipsum dolor sit amet, <strong>consectetur adipiscing elit.</strong> Ut semper, ante sed blandit porta, ligula urna sodales massa, sed viverra odio lorem eu lacus. Nunc pellentesque orci nec est tempus interdum. Aliquam auctor velit a quam egestas ornare. Curabitur tristique dolor ac justo efficitur, et mattis <a href='#'>lectus tempor.</a> Mauris vitae purus molestie, molestie metus volutpat, volutpat ipsum. Phasellus in turpis tellus. Nam eget tortor auctor nunc vestibulum bibendum a vitae lacus. Etiam libero arcu, accumsan vel ex vulputate, tincidunt euismod dolor. Sed interdum, tortor ut tincidunt pretium, metus enim ultrices elit, eu ultricies nulla ipsum vel justo. Morbi imperdiet rutrum nibh non vehicula. Maecenas at vulputate felis, eget pretium lacus. Morbi mauris ex, aliquam eget sodales in, semper a lorem. Donec porta suscipit orci, a placerat leo ornare feugiat. Aliquam feugiat, ligula et tincidunt suscipit, ipsum augue dictum augue, ac sollicitudin nisi lectus eu elit. Nullam condimentum, urna eget condimentum vestibulum, justo ante consectetur mi, eu ultricies nisl nisi vitae odio.<br/><br/>
 						Mauris consectetur tellus sem, 
-					<strong>strong text consectetur bibendum neque rhoncus a.</strong> Praesent <a href='grbrbr'>imperdiet metus</a> orci, consectetur aliquam ipsum molestie a. Praesent pulvinar ut ex dapibus vulputate. Etiam ut vestibulum velit. Mauris vitae ante vel turpis placerat suscipit nec sed ipsum. Nulla facilisi. Quisque gravida, lectus sed fringilla molestie, felis turpis sollicitudin erat, fringilla congue tortor dolor pharetra mi. Vestibulum a sem ornare lacus hendrerit molestie non at justo. Etiam dictum libero eget hendrerit dignissim. Aenean maximus maximus risus, a luctus nisi convallis non. Nunc sed ipsum massa. Nulla mollis est tellus, vulputate accumsan quam pharetra sit amet. <br/><br/>
+						<strong>strong text consectetur bibendum neque rhoncus a.</strong> Praesent <a href='grbrbr'>imperdiet metus</a> orci, consectetur aliquam ipsum molestie a. Praesent pulvinar ut ex dapibus vulputate. Etiam ut vestibulum velit. Mauris vitae ante vel turpis placerat suscipit nec sed ipsum. Nulla facilisi. Quisque gravida, lectus sed fringilla molestie, felis turpis sollicitudin erat, fringilla congue tortor dolor pharetra mi. Vestibulum a sem ornare lacus hendrerit molestie non at justo. Etiam dictum libero eget hendrerit dignissim. Aenean maximus maximus risus, a luctus nisi convallis non. Nunc sed ipsum massa. Nulla mollis est tellus, vulputate accumsan quam pharetra sit amet. <br/><br/>
 						Duis sed ultrices magna,<span>sit amet pretium erat.</span>  In nibh arcu, ullamcorper a ipsum at, faucibus viverra nunc. Quisque porta elementum sapien varius tincidunt. In hac habitasse platea dictumst. Sed aliquet non dolor eu interdum. Etiam pretium aliquam libero, non luctus arcu consequat ut. Maecenas mollis gravida lectus vel elementum. <br/><br/>
 
-					<h2>Subtitle</h2>
+						<h2>Subtitle</h2>
 
 						In facilisis, enim eu suscipit consectetur, elit ipsum <strong>ullamcorper quam,</strong> at posuere leo lorem id libero. Vestibulum luctus felis quam, id porttitor lacus rutrum eget. Nullam auctor varius nisl gravida hendrerit. Nulla facilisi. Cras placerat porttitor dictum. Vestibulum non libero iaculis, gravida est vel, consequat nulla. Vivamus aliquam congue quam, in sollicitudin mauris interdum id. Vivamus sapien lacus, ornare id metus sit amet, aliquam bibendum urna. Sed varius dui sit amet eros suscipit rhoncus. Nullam laoreet euismod lectus. In hac habitasse platea dictumst. <br/><br/>
 						Quisque congue accumsan justo, cursus rutrum arcu sodales eu. Nam at lorem a est venenatis maximus. Mauris fringilla arcu interdum dapibus venenatis. Quisque tempor dui justo, ut mollis purus fringilla ac. Morbi quis lectus sit amet velit tempus venenatis vitae vitae risus. In dictum tortor turpis, sed mattis dui convallis eu. Sed convallis nibh ipsum, a suscipit leo hendrerit vel. Nam ullamcorper leo risus, dapibus pharetra lorem efficitur at. Praesent vel libero sit amet lacus rhoncus auctor. Curabitur molestie est sed nibh porttitor, nec condimentum libero pellentesque. <br/><br/>
-					<h3>subtitle</h3>
+						<h3>subtitle</h3>
 						Integer ex sapien, suscipit ac risus vel, semper dictum risus. Nunc libero ligula, mollis a viverra vitae, consequat at sapien. Donec condimentum magna in ullamcorper rhoncus. Mauris lobortis porta metus nec congue. Sed nisl dui, dapibus ut dolor ut, tincidunt porttitor tortor. Praesent efficitur, orci a imperdiet fermentum, neque ante fermentum elit, at iaculis elit sapien non tortor. Mauris turpis tortor, lacinia non posuere in, tincidunt id metus. Suspendisse potenti. Quisque pulvinar, urna a feugiat lacinia, ligula dui vehicula nunc, quis elementum neque risus ut nunc. Phasellus sed magna fringilla metus convallis finibus. Mauris nec placerat libero, at consequat purus. Mauris aliquam ligula fringilla tellus mattis tempor. Nulla pulvinar ultrices lacus. Cras sollicitudin erat erat, sed tristique mauris tempus quis. Duis at consequat magna. <br/><br/>
 
 					</p>
@@ -86,7 +114,7 @@
 				</div>
 
 				<div id='footer'> 
-					<p>this is the fucking footer nigga</p>
+					<p>this is the footer</p>
 				</div>
 
 
@@ -95,16 +123,22 @@
 
 
 
+			<div id="demo"></div>
 
-		</div>
+		<!--<div class='floater floater_first'>this is a test floater</div>
 
-
-
-
-
+		<div class='floater'>this is a test floater</div> -->
 
 
 	</div>
+
+
+
+
+
+
+
+</div>
 
 
 
